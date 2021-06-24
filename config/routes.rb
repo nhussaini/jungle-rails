4f65229de695ae1a166c0202dd32d991c0e83f2b
unknown <nasrullah_hussaini@yahoo.com>
2021-06-24 10:39:11 -0700

    add register form

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,9 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :new, :create]
   end
+   # get '/signup' => 'users#new'
+  # post '/users' => 'users#create'
+  resources :users, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
